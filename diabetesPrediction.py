4c0c77626ba0cc067e789922069e44fb58bb7f23
ayush kumar <ayushkumar19999@gmail.com>
2025-09-25 21:54:07 +0530

    simpleimputer columntransformer diabetes prediction system added

---
+++ diabetesPrediction.py
@@ -0,0 +1,62 @@
+import pickle
+import streamlit as st
+import pandas as pd
+
+
+
+
+model = pickle.load(open("diabetesModel.pkl","rb"))
+transformer = pickle.load(open("diabetesTransformer.pkl","rb"))
+
+st.title("Diabetes Prediction")
+
+gender  = st.selectbox("Select Gender",options=["Male","Female","Other"])
+age = st.number_input("Enter age")
+hypertension_input = st.selectbox("Do you have Hypertension",options=["Yes","No"])
+heart_disease_input = st.selectbox("Do you have Heart disease",options=["Yes","No"])
+smoking_history = st.selectbox("Smoking History",options=["No Info","never","former","current", "not current","ever"])
+BMI = st.number_input("Enter BMI")
+hba1c_level = st.number_input("Enter HbA1c level")
+glucose_level = st.number_input("Enter Blood glucose level")
+
+#convert hypertension and heart_disease into 0/1
+
+hypertension = 1 if hypertension_input == "Yes" else 0
+heart_disease = 1 if heart_disease_input == "Yes" else 0
+
+input_dict = {
+    "gender":gender,
+    "age":age,
+    "hypertension":hypertension,
+    "heart_disease":heart_disease,
+    "smoking_history":smoking_history,
+    "bmi":BMI,
+    "HbA1c_level":hba1c_level,
+    "blood_glucose_level":glucose_level,
+}
+
+def predict_diabetes(input_dict,transformer):
+
+    #Convert the dictionary into a single frame
+
+    input_df = pd.DataFrame([input_dict])
+
+    #Transform using the fitted transformer
+
+    input_tf = transformer.transform(input_df)
+
+    predict = model.predict(input_tf)
+
+    return predict[0]
+
+if st.button("Check Now"):
+    prediction = predict_diabetes(input_dict,transformer)
+
+    if prediction == 0:
+        st.markdown("<h4 style='color:green'>You are not currently at risk of diabetes.</h4>", unsafe_allow_html=True)
+    else:
+        st.markdown(
+            "<h4 style='color:red'>Based on the input data, there is a risk of diabetes. Please consult a healthcare professional.</h4>",
+            unsafe_allow_html=True)
+
+
